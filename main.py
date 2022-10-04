d5cdf015bb2e9e6128fe3959102c52b5de45d26a
TaylorWhite-11 <824835@mystma.org>
2022-10-04 14:23:57 -0500

    1

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def topFiveMovies(a,b,c,d,e):
-    return a + " " + b+" "+ c +" "+ d +" " + e
- 
-print(topFiveMovies("Jaws","Dark knight","star wars","Spider man","The hulk"))
-    
+def Helloworld():
+    print("Helloworld")    
+Helloworld()
